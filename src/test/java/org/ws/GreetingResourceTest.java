131f61750d04b8187a72b5d9e7fdf8584e7171d8
Tiago Marques <tmarques@dlocal.com>
2022-11-03 01:26:28 -0300

    fix test

--- src/test/java/org/ws/GreetingResourceTest.java
+++ src/test/java/org/ws/GreetingResourceTest.java
@@ -12,10 +12,9 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .when().get("/q/health")
           .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .statusCode(200);
     }
 
 }
